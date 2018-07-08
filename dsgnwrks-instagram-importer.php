22fb612dd38f670ca9ce3f24a9de685b4374fc2b
Denis Romaniuk <dionysius.stealth@gmail.com>
2018-07-08 22:16:45 +0300

    Version number modified

--- dsgnwrks-instagram-importer.php
+++ dsgnwrks-instagram-importer.php
@@ -6,7 +6,7 @@ Description: Allows you to backup your instagram photos while allowing you to ha
 Author URI: http://dsgnwrks.pro
 Author: DsgnWrks
 Donate link: http://dsgnwrks.pro/give/
-Version: 1.4.1
+Version: 1.4.1.1
 */
 
 class DsgnWrksInstagram extends DsgnWrksInstagram_Debug {
